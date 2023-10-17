5d58d58e32ee258e6f5ef07d0296ca1375a85ddc
Pras Pitasawad <praspitasawad@DodoMackai.local>
2023-10-17 12:11:49 +0700

    implement training logic & FastAPI for evotune service

---
+++ internal/evotune-microservice/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+
+from pathlib import Path
+
+from jax.random import PRNGKey
+
+from jax_unirep import evotune
+from jax_unirep.evotuning_models import mlstm64
+from jax_unirep.utils import dump_params
+
+from src.model.model import requestEvotuneBody
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/evotune")
+def request_evotune(requestBody: requestEvotuneBody):
+    # Print Request Body
+    print(requestBody)
+
+    # get train set 
+    sequences = ["HASTA", "VISTA", "ALAVA", "LIMED", "HAST", "HAS", "HASVASTA"] * 5
+
+    # get validation set
+    holdout_sequences = [
+        "HASTA",
+        "VISTA",
+        "ALAVA",
+        "LIMED",
+        "HAST",
+        "HASVALTA",
+    ] * 5
+
+    init_fun, apply_fun = mlstm64()
+    # The input_shape is always going to be (-1, 26),
+    # because that is the number of unique AA, one-hot encoded.
+    _, inital_params = init_fun(PRNGKey(42), input_shape=(-1, 26))
+
+    # 1. Evotuning with Optuna
+    # n_epochs_config = {"low": 1, "high": 1}
+    # lr_config = {"low": 1e-5, "high": 1e-3}
+    study, evotuned_params = evotune(
+        sequences=sequences,
+        model_func=apply_fun,
+        params=inital_params,
+        out_dom_seqs=holdout_sequences,
+        n_trials=requestBody.evotune_params.n_trials,
+        n_splits=requestBody.evotune_params.n_splits,
+        n_epochs_config=requestBody.evotune_params.n_epochs_config,
+        learning_rate_config=requestBody.evotune_params.learning_rate_config,
+    )
+
+    # store weight in evotuned_weights_url
+    # dump_params(evotuned_params, Path(PROJECT_NAME))
+    # print("Evotuning done! Find output weights in", PROJECT_NAME)
+    return{"success":"true"}
---
+++ internal/evotune-microservice/src/model/model.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, HttpUrl
+
+class epochs_config(BaseModel):
+    low:  int
+    high: int
+
+class lr_config(BaseModel):
+    low:  float
+    high: float
+
+class EvotuneParams(BaseModel):
+    n_trials: int | None = 2
+    n_splits: int | None = 2
+    n_epochs_config: epochs_config | None = {"low":1, "high":1}
+    learning_rate_config: lr_config | None = {"low":1e-5, "high":1e-3}
+
+class requestEvotuneBody(BaseModel):
+    job_id: str
+    dataset_url: HttpUrl
+    evotuned_weights_url: HttpUrl
+    evotune_params: EvotuneParams
