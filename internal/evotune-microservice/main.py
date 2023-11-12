2761d76843b6d470283b6788a76489daa9f3c106
praspit <praspit@outlook.com>
2023-11-12 21:06:30 +0700

    merge conflict

---
+++ internal/evotune-microservice/main.py
@@ -0,0 +1,42 @@
+import threading
+import sys
+sys.path.append("../")
+from dotenv import load_dotenv
+load_dotenv()
+
+from fastapi import FastAPI
+from aiofile import AIOFile
+
+from common.db import createBucket, uploadToBucket, downloadFromBucket
+from src.model.model import RequestEvotuneBody
+from src.service.train import runEvotuneThread
+
+app = FastAPI()
+
+@app.get("/createBucket")
+def bucketCreation():
+    print("----------------------------------------------------------")
+    print("creating bucket...")
+    createBucket("unirep")
+    print("bucket created")
+
+@app.get("/evotune")
+def get_weights():
+    print("----------------------------------------------------------")
+    print("getting evotuned_params...")
+    downloadFromBucket("unirep", "123", "1.pkl")
+    return "success"
+
+@app.post("/evotune")
+def requestEvotune(requestBody: RequestEvotuneBody):
+    print("----------------------------------------------------------")
+    print("requesting evotune service...")
+    print(requestBody)
+    try:
+        # Create and start the BLAST thread
+        evotuneThread = threading.Thread(target=runEvotuneThread, args=(requestBody,))
+        evotuneThread.start()
+
+        return {"run_evotune_thread":"success"}
+    except:
+        return {"run_evotune_thread":"fail"}
