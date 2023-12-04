e4e65648a7e3b28a56ecfd05e5c7726a0f7ccf9b
sirapoom leesuravanich <sirapoomlee@gmail.com>
2023-12-04 15:11:44 +0700

    connect fittop with rabbit

---
+++ internal/common/mq.py
@@ -0,0 +1,31 @@
+import time
+
+from common.publisher import publishJobStatusEvent, JobStatusEventMessage, JobUpdateData, Artifact
+
+def publishCompletedJobStatusToMQ(serviceName, bucketName, stageID, jobId, filePath):
+    message = JobStatusEventMessage(
+        service_name=serviceName,
+        timestamp=time.time(),
+        data=JobUpdateData(
+            job_id=jobId,
+            stage_id=stageID,
+            status="COMPLETED",
+            artifact=Artifact(bucket_name=bucketName, path=filePath),
+            error="",
+        ),
+    )
+    publishJobStatusEvent(message)
+
+def publishFailedJobStatusToMQ(serviceName, bucketName, stageID, job_id, filePath, error):
+    message = JobStatusEventMessage(
+        service_name=serviceName,
+        timestamp=time.time(),
+        data=JobUpdateData(
+            job_id=job_id,
+            stage_id=stageID,
+            status="FAILED",
+            artifact=Artifact(bucket_name=bucketName, path=filePath),
+            error=error,
+        ),
+    )
+    publishJobStatusEvent(message)
--- internal/fittop-microservice/src/service/top_model_utils.py
+++ internal/fittop-microservice/src/service/top_model_utils.py
@@ -1,6 +1,13 @@
 from Bio import SeqIO
 import numpy as np
 import feather
+import pandas as pd
+from jax_unirep import get_reps, fit
+from jax_unirep.utils import load_params
+from common.db import downloadFromBucket,uploadToBucket
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.linear_model import RidgeCV, LinearRegression, HuberRegressor
+import pickle as pkl
 
 global PATH
 PATH = '.'
@@ -133,3 +140,78 @@ def confusion_matrix_loss(Y_test,Y_preds_test):
   loss = np.sum(confusion_matrix)/confusion_matrix.size
 
   return loss
+
+def loadData():
+   return pd.DataFrame(read_labeled_data('example'), columns = ['sequence', 'fitness'])
+def loadSeqs(seqs_df,bucket_name,model_path):
+    N_seqs = len(seqs_df)
+    N_BATCHES = min(max(6, N_seqs // 500), N_seqs)
+    BATCH_LEN = int(np.ceil(N_seqs / N_BATCHES))
+
+    param= pkl.loads(downloadFromBucket(bucket_name, model_path))[1]
+
+    # get 1st sequence
+    reps, _, _ = get_reps(seqs_df.sequence[0], params=param,mlstm_size=64)
+    feat_cols = ['feat' + str(j) for j in range(1, reps.shape[1] + 1)]
+    this_df = pd.DataFrame(reps, columns=feat_cols)
+    this_df.insert(0, "sequence", seqs_df.sequence[0])
+    this_df.insert(1, "fitness", seqs_df.fitness[0])
+    for i in range(N_BATCHES):
+        this_unirep, _, _ = get_reps(
+            seqs_df.sequence[(1 + i * BATCH_LEN):min(1 + (i + 1) * BATCH_LEN, N_seqs)],
+            params=param,mlstm_size=64)
+        this_unirep_df = pd.DataFrame(this_unirep, columns=feat_cols)
+        this_unirep_df.insert(0, "sequence",
+                                seqs_df.sequence[(1 + i * BATCH_LEN):min(1 + (i + 1) * BATCH_LEN, N_seqs)].reset_index(
+                                    drop=True))
+        this_unirep_df.insert(1, "fitness",
+                                seqs_df.fitness[
+                                (1 + i * BATCH_LEN):min(1 + (i + 1) * BATCH_LEN, N_seqs)].reset_index(drop=True))
+        this_df = pd.concat([this_df.reset_index(drop=True), this_unirep_df.reset_index(drop=True)]).reset_index(
+            drop=True)
+    return this_df
+
+def doRidgeRegression(this_df, TRAIN_BATCH_SIZES, N_BATCH, N_RAND_BATCHES, WT_FIT, ALPHA):
+    batch_level = []
+    for TRAIN_BATCH_SIZE in TRAIN_BATCH_SIZES:
+        HOLDOUT_BATCH_SIZE = TRAIN_BATCH_SIZE * 10
+
+        params_level = []
+        df = this_df
+
+        scores_level = []
+        for i in range(N_BATCH):
+            np.random.seed(42 * (i + 2))
+            rndperm = np.random.permutation(df.shape[0])
+
+            X = df.loc[rndperm[0:TRAIN_BATCH_SIZE], df.columns[2:]]
+            Y = df.loc[rndperm[0:TRAIN_BATCH_SIZE], "fitness"]
+
+            X_holdout = df.loc[rndperm[TRAIN_BATCH_SIZE:TRAIN_BATCH_SIZE + HOLDOUT_BATCH_SIZE], df.columns[2:]]
+            Y_holdout = df.loc[rndperm[TRAIN_BATCH_SIZE:TRAIN_BATCH_SIZE + HOLDOUT_BATCH_SIZE], "fitness"]
+
+            kfold = KFold(n_splits=10, shuffle=True)
+
+            model = RidgeCV(alphas=[ALPHA], cv=kfold)
+
+            model.fit(X, Y)
+
+            Y_preds = model.predict(X_holdout)
+
+            usorted = np.array(Y_holdout)[np.argsort(Y_preds)][::-1][:int(HOLDOUT_BATCH_SIZE / 10)]
+
+            usorted_count = np.sum([1 if i > WT_FIT else 0 for i in usorted])
+
+            avg_rand_count = 0
+            for k in range(N_RAND_BATCHES):
+                np.random.seed(42 * (i + 2) + (1 + k))
+                rand_Y = np.random.permutation(np.array(Y_holdout))[:int(HOLDOUT_BATCH_SIZE / 10)]
+                avg_rand_count += np.sum([1 if i > WT_FIT else 0 for i in rand_Y])
+            avg_rand_count /= N_RAND_BATCHES
+
+            scores_level.append(usorted_count / avg_rand_count)
+
+        params_level.append(scores_level)
+        batch_level.append(params_level)
+
+    return model
