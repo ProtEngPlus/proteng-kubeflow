ed4b728fbe5b9a37c9753f59725f2ef93088f8c0
bbompk <panithiv@gmail.com>
2024-02-24 15:28:58 +0700

    feat: logging route class

--- projects/mutation/microservice.py
+++ projects/mutation/microservice.py
@@ -4,7 +4,7 @@ sys.path.append("../../")
 from dotenv import load_dotenv
 load_dotenv()
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 
@@ -12,14 +12,17 @@ from src.service.thread import runMutationThread
 from src.model.model import RequestMutationBody
 
 from pkg.common.logger import mutationLogger as logger
+from pkg.common.logger import getLogger
+from pkg.common.fast_api import GetLoggingRouteClass
 
 app = FastAPI()
+router = APIRouter(route_class=GetLoggingRouteClass(getLogger('FastAPI')))
 
 @app.exception_handler(HTTPException)
 def http_exception_handler(req, e):
     return JSONResponse({"code": 500, "error": str(e)}, 500)
 
-@app.post("/mutation")
+@router.post("/mutation")
 def requestEvotune(requestBody: RequestMutationBody):
     logger.debug("----------------------------------------------------------")
     logger.debug("requesting mutation service...")
@@ -32,3 +35,5 @@ def requestEvotune(requestBody: RequestMutationBody):
         return {"code": 200, "message": "started mutation thread"}
     except Exception as e:
         raise e
+    
+app.include_router(router)
