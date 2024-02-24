e47ab2d4a47d41b48ebd35bc6679c2b43a1cdb83
bbompk <panithiv@gmail.com>
2024-02-24 15:29:14 +0700

    feat: logging route class

---
+++ pkg/common/fast_api.py
@@ -0,0 +1,17 @@
+from fastapi import Request, Response
+from fastapi.routing import APIRoute
+from typing import Callable
+
+def GetLoggingRouteClass(logger):
+    class CustomRoute(APIRoute):
+        def get_route_handler(self) -> Callable:
+            original_route_handler = super().get_route_handler()
+
+            async def custom_route_handler(request: Request) -> Response:
+                body = await request.body()
+                logger.info(f"method={request.method} path={request.url.path} headers={request.headers} body={body.decode('utf-8')} query_params={request.query_params}")
+                response: Response = await original_route_handler(request)
+                return response
+
+            return custom_route_handler
+    return CustomRoute
