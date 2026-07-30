0d23c0db2f2bcc62523b0ebdfa9330e327c15371
BadLuckZ <thanagorn8802@gmail.com>
2026-07-31 01:59:12 +0700

    fix: consumer

--- projects/evotune/consumer.py
+++ projects/evotune/consumer.py
@@ -65,6 +65,7 @@ async def main(loop):
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     loop.run_until_complete(main(loop))
     loop.close()
